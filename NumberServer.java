79849698fb276d80207a03b4b165c366202bb8a2
cacasares <cacasares@ucsd.edu>
2023-01-29 11:25:25 -0800

    Update NumberServer.java
    edits

--- NumberServer.java
+++ NumberServer.java
@@ -4,19 +4,31 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
+    int num = 0;
+    String inputs="";
 
     public String handleRequest(URI url) {
-        String inputs="";
-        if (url.getPath().contains("/add-message?s=")) {
-            inputs= url.getQuery().split("=") + "\n";
-            //System.out.println(inputs);
-            return inputs;
+        if (url.getPath().equals("/")) {
+            return String.format("Please enter a string: ");
+        } else if (url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            inputs+= parameters[1]+ "\n";
+            return String.format(inputs);
+        } else {
+            System.out.println("Path: " + url.getPath());
+            // if (url.getPath().contains("/add")) {
+            //     String[] parameters = url.getQuery().split("=");
+            //     if (parameters[0].equals("count")) {
+            //         num += Integer.parseInt(parameters[1]);
+            //         return String.format("Number increased by %s! It's now %d", parameters[1], num);
+            //     }
+            // }
+            return "404 Not Found!";
         }
-        return "404 Not Found!";
     }
+}
 
-
-class StringServer {
+class NumberServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
@@ -28,7 +40,35 @@ class StringServer {
         Server.start(port, new Handler());
     }
 }
-}
+
+// class Handler implements URLHandler {
+//     // The one bit of state on the server: a number that will be manipulated by
+//     // various requests.
+
+//     public String handleRequest(URI url) {
+//         String inputs="";
+//         if (url.getPath().contains("/add-message?s=")) {
+//             inputs= url.getQuery().split("=") + "\n";
+//             //System.out.println(inputs);
+//             return inputs;
+//         }
+//         return "404 Not Found!";
+//     }
+
+
+// class StringServer {
+//     public static void main(String[] args) throws IOException {
+//         if(args.length == 0){
+//             System.out.println("Missing port number! Try any number between 1024 to 49151");
+//             return;
+//         }
+
+//         int port = Integer.parseInt(args[0]);
+
+//         Server.start(port, new Handler());
+//     }
+// }
+// }
 
 // import java.io.IOException;
 // import java.net.URI;
